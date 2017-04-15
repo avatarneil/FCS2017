8592d206f5d0dff30c8a15112c98d8efab5d2a9d
Neil Goldader <Neil Goldader>
2017-04-15 13:08:51 -0400

    some coefficient changes

--- eulers.py
+++ eulers.py
@@ -23,17 +23,17 @@ def eulers():
     h=0
     omega=(2*np.pi)/24
     u=.01
-    
+    o=5
     while t<=120:
         thresh=thresh+0.7*np.random.randn()
         dcdt = mu*(c*omega**2-(1/3)*c**3-c2)
         dc2dt = ((1/mu)*c)*omega**2
         c=c+dcdt*dt
         c2=c2+dc2dt*dt
-        if (h-c>=thresh and sleepstatus==1):
-            sleepstatus=0
-        elif(np.abs(h-c)<.1 and sleepstatus==0):
-            sleepstatus=1
+#        if (h-c>=thresh and sleepstatus==1):
+#            sleepstatus=0
+#        elif(np.abs(h-c)<.1 and sleepstatus==0):
+#            sleepstatus=1
         
         if(sleepstatus==1):
             dhdt=(-h/xw)+(hasy/xw)
@@ -46,14 +46,14 @@ def eulers():
         carray.append(c)
         c2array.append(c2)
         tarray.append(t)
-        alertarray.append(h-c)
+        alertarray.append((o*(np.e**-h)*c)+h)
         t=t+dt
         i=i+1
         thresh=3
 eulers()
-#plt.plot(tarray,alertcarray,'purple',label='hc')
-plt.plot(tarray,carray,'r',label='C')
-plt.plot(tarray,harray,'g',label='h')
+plt.plot(tarray,alertarray,'purple',label='hc')
+#plt.plot(tarray,carray,'r',label='C')
+#plt.plot(tarray,harray,'g',label='h')
 plt.legend(loc='best')
 plt.xlabel('t')
 plt.grid()
